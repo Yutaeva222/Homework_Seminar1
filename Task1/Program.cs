32db064c6552028ff5e00255644a0c14a3ab10b5
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-16 19:32:43 +0300

    Задача 1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine("Большее число " + a);
+    Console.WriteLine("Меньшее число " + b);
+}
+else
+{
+    Console.WriteLine("Большее число " + b);
+    Console.WriteLine("Меньшее число " + a);
+}
