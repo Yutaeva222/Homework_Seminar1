bb41f7af6e5eb117271d30bb47039eacfdf6d7fc
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-16 21:49:51 +0300

    Задача2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(b>max)
+{
+    max = b;
+}
+else
+{
+    max = a;
+}
+if(c>max)
+{
+    max = c;
+}
+Console.WriteLine("Максимальное число: " + max);
