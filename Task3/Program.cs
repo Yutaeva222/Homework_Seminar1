19cef9ec1aa31d83bca4293be8e79d6022c75f72
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-18 11:20:21 +0300

    задача3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a % 2 == 1)
+{
+    Console.WriteLine("Это нечетное число. ");
+}
+else
+{
+    Console.WriteLine("Это число четное.");
+}
