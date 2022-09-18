d12d50acf72f3405b102ddfad16c242a16268278
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-18 11:25:11 +0300

    изменила немного код

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите любое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-if(a % 2 == 1)
+if(a % 2 == 0)
 {
-    Console.WriteLine("Это нечетное число. ");
+    Console.WriteLine("Это четное число. ");
 }
 else
 {
-    Console.WriteLine("Это число четное.");
+    Console.WriteLine("Это нечисло четное.");
 }
