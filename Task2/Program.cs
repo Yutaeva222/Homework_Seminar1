1e5151de03edca1c1cd441a292d93076b8d25f3d
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-20 21:59:04 +0300

    название задачи

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Clear();
+﻿// Из трех числе найти максимальное число.
+Console.Clear();
 Console.WriteLine("Введите первое число:");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
