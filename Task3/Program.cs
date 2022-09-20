1d5cad83ee55f18f56185df3b06dfeaa1b17e805
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-20 22:54:20 +0300

    исправления

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,11 +1,12 @@
-﻿Console.Clear();
+﻿// Является ли число четным.
+Console.Clear();
 Console.WriteLine("Введите любое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 if(a % 2 == 0)
 {
-    Console.WriteLine("Это четное число. ");
+    Console.WriteLine($"{a} это четное число.");
 }
 else
 {
-    Console.WriteLine("Это нечетное число.");
+    Console.WriteLine($"{a} это НЕчетное число.");
 }
